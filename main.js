eedd6459bbdad240cc4cd563ede6f5b203b2c949
Apple <tiffine.koch@gmail.com>
2016-01-20 12:28:05 -0800

    updated files

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 'use strict'
 
-// var timestamp = Date.now();
 var ref = new Firebase('https://gameredux.firebaseio.com/');
 console.log(ref);
 var amOnline = ref.child('info/connected');
-// var usersRef = ref.child('users');
 var playersRef = ref.child('players');
-// var userID = usersRef.child(timestamp);
 var gameRef = ref.child('game_state');
-var myTurn = true;
-var currentPlayerRef = ref.child('currentPlayer');
-var player, currentPlayer, myTurn;
-var $gameBoard;
+var playerTurn = true;
+var player, mark;
 
+// var currentPlayerRef = ref.child('currentPlayer');
 
 $(document).ready(init);
-//   $('#start').on('click', startGame);
-// });
 
 function init() {
   $('#username').click(enterName);
-  //psuedocode
   console.log('hi');
-  $('.box').click(markSquare);
-  //monitor
+  $('.square').click(clickSquare);
   ref.child('turn').on('value', function(snap) {
     var playerTurn = snap.val();
     if (!playerTurn) {
@@ -36,44 +28,46 @@ function startGame() {
   ref.child('turn').set('player1');
   var squareArray = [0,0,0,0,0,0,0,0,0];
   gameRef.set(squareArray);
-  // var userid = $('#start').val();
-  // var userid = $('#start').val();
-  markSquare();
+
+  clickSquare();
   console.log('working');
 }
 
-function markSquare(){
-  if(myTurn !== player) {
-    return;
-  } if (myTurn === player)
-    if (player1 = true) {
+function clickSquare() {
+currentPlayerRef.once('value', function(snapshot) {
+  if (playerTurn === player){  // is it my turn
     var index = $(this).attr('data');
-    gameRef.once('value', function(snapshot) {
-      var state = snap.val();
-      state[index - 1] = 1;
-      gameRef.set(state);
-    } ref.child('turn').set(player2);
-  } if(player2 = true) {
-    var index = $(this).attr('data');
-    gameRef.once('value', function(snapshot) {
-      var state = snap.val();
-      state[index - 1] = 2;
-      gameRef.set(state);
     }
-  } ref.child('turn').set(player1);
+  })
+    var newState = {};
+    newState[index] = player === player1 ? 1 : 0;
+    gameRef.update(newState);
+    var mark = $(this).text();
+    if (player === player1) {
+      mark = "Exes";
+    } else {
+      mark = "Ohs";
+    }
+    ref.child('turn').set(player === player1 ? player2 : player1);
   }
-}
+
 function enterName() {
-  // $('#username').val();
+  $('#username').val();
   playersRef.once('value', function(snapshot) {
-    // console.log(snapshot.val());
+
     if(!snapshot.val()) {
       playersRef.push($({player1: $('#username').val()}));
       player = 'player1';
+      var currentPlayer = "Exes";
+      console.log(currentPlayer);
+      debugger;
+      $('#namePlayer').text("You are" + currentPlayer);
     }
     else if (Object.keys(snapshot.val()).length === 1) {
       playersRef.push({player2: $('#username').val()});
       player = 'player2';
+      var currentPlayer = "Ohs";
+      $('#namePlayer').text("You are" + currentPlayer);
       startGame();
     }
     else {
@@ -82,7 +76,14 @@ function enterName() {
   });
 }
 
-function checkForWin() {
-
-  var possibleWins = [[1,2,3],[3,6,9],[7,8,9],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[4,5,6]];
-}
+// function checkForWin() {
+//
+//   var possibleWins = [[1,2,3],[3,6,9],[7,8,9],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[4,5,6]];
+//   var $messageWin = "You Win";
+//   $('h1').append($messageWin).addClass('header');
+//   resetGame();
+// }
+//
+// function resetGame() {
+//
+// }
